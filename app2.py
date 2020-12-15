7c2e8806fa2b64ed78e149098447668c092d36f0
Jose Galisteo <ceritium@gmail.com>
2020-12-15 14:26:00 +0100

    Read from file

--- app2.py
+++ app2.py
@@ -6,6 +6,7 @@ import signal
 import sys
 import urwid
 import pyttsx3
+import unicodedata
 
 def sayFunc(phrase):
     engine = pyttsx3.init()
@@ -24,47 +25,26 @@ def main():
     Game()
 
 class Game:
-    DICT = [
-            'VERONICA',
-            'GATO',
-            'PATO',
-            'PERRO',
-            'DEDO',
-            'PELOTA',
-            'MOCO',
-            'PEDO',
-            'DADO',
-            'CAMINO',
-            'PAPA',
-            'MAMA',
-            'CAMISA',
-            'VASO',
-            'ROJO',
-            'ROSA',
-            'GATA',
-            'PERRA',
-            'PATA',
-            'CAMA',
-            'MESA',
-            'PESA',
-            'OJO',
-            'AJO',
-            'HOJA',
-            'PERA',
-            ]
-
+    DICT = list(map(lambda x: x.strip().upper(), open('simple_words.txt').readlines()))
     MISSING_LETTER = '_'
     VOWELS = 'AEIOU'
 
     def __init__(self):
         self.text = None
-        self.pair = None
+        self.word = None
         self.txt = urwid.Text("", align='center')
         fill = urwid.Filler(self.txt, 'middle')
         self.loop = urwid.MainLoop(fill, unhandled_input=self.handle_input)
 
         self.new_round()
         self.loop.run()
+
+    @classmethod
+    def strip_accents(cls, text):
+        text = unicodedata.normalize('NFD', text)\
+            .encode('ascii', 'ignore')\
+            .decode("utf-8")
+        return str(text)
 
     @classmethod
     def text_decorator(cls, string, kind):
@@ -93,7 +73,7 @@ class Game:
         return "".join(list(map(Game.replace_letter, word)))
 
     def alarm_reset_text(self, _loop=None, _data=None):
-        self.text = self.pair[1]
+        self.text = self.word['challenge']
         self.txt.set_text(self.text)
 
     def alarm_new_round(self, _loop=None, _data=None):
@@ -101,30 +81,32 @@ class Game:
 
     def new_challenge(self):
         word = random.choice(Game.DICT)
-        challenge = Game.replace_word(word)
-        while challenge == word:
-            challenge = Game.replace_word(word)
+        screen = Game.strip_accents(word)
 
-        self.pair = (word, challenge)
+        challenge = Game.replace_word(screen)
+        while challenge == screen:
+            challenge = Game.replace_word(screen)
+
+        self.word = {'speak': word, 'screen': screen, 'challenge': challenge}
 
     def new_round(self):
         self.new_challenge()
-        self.text = self.pair[1]
+        self.text = self.word['challenge']
         self.txt.set_text(self.text)
-        Game.say(self.pair[0])
+        Game.say(self.word['speak'])
 
     def handle_input(self, key):
         if isinstance(key, str) and len(key) == 1:
             self.text = self.text.replace(Game.MISSING_LETTER, key.upper(), 1)
             self.txt.set_text(self.text)
             if(not(Game.MISSING_LETTER in self.text)):
-                if self.pair[0] == self.text:
+                if self.word['screen'] == self.text:
                     self.txt.set_text(self.text_decorator(self.text, "success"))
                     self.loop.set_alarm_in(1, self.alarm_new_round)
                 else:
                     self.txt.set_text(self.text_decorator(self.text, "error"))
                     self.loop.set_alarm_in(2, self.alarm_reset_text)
-                    Game.say(self.pair[0])
+                    Game.say(self.word['speak'])
 
 if __name__=="__main__":
     main()
