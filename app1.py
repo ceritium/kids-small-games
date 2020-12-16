81339581fa441c7fe313b990fc31225d6b8db7fd
Jose Galisteo <ceritium@gmail.com>
2020-12-16 09:59:02 +0100

    Adding some styling to app1.py

--- app1.py
+++ app1.py
@@ -10,9 +10,31 @@ import utils
 class Game:
     def __init__(self):
         self.text = "HOLA!"
+
+        palette = [
+            ('titlebar', 'black', 'white'),
+            ('enter button', 'dark green,bold', 'black'),
+            ('quit button', 'dark red,bold', 'black'),
+            ('getting quote', 'dark blue', 'black')]
+
+        header_text = urwid.Text(u'FREE TEXT TO SPEECH')
+        header = urwid.AttrMap(header_text, 'titlebar')
+
+        menu = urwid.Text([
+            u'Press (', ('enter button', u'ENTER'), u') to speech. ',
+            u'Press (', ('quit button', u'CTRL-C'), u') to quit. ',
+            ])
+
         self.txt = urwid.Text(self.text, align='center')
-        fill = urwid.Filler(self.txt, 'middle')
-        loop = urwid.MainLoop(fill, unhandled_input=self.handle_input)
+        quote_filler = urwid.Filler(self.txt, valign='top', top=1, bottom=1)
+        v_padding = urwid.Padding(quote_filler, left=1, right=1)
+        quote_box = urwid.LineBox(v_padding)
+
+        layout = urwid.Frame(header=header, body=quote_box, footer=menu)
+
+        # Create the event loop
+        loop = urwid.MainLoop(
+                layout, palette, unhandled_input=self.handle_input)
         loop.run()
 
     def handle_input(self, key):
@@ -36,5 +58,6 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
     Game()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
